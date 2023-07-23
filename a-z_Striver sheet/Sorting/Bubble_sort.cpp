36d91f2e70bceb91830b3d082c0c0b21506f01c1
PalakMaheshwari20 <pmaheshwari.lakhotia@gmail.com>
2023-07-23 22:53:17 +0530

    23/07

--- a-z_Striver sheet/Sorting/Bubble_sort.cpp
+++ a-z_Striver sheet/Sorting/Bubble_sort.cpp
@@ -5,12 +5,19 @@ using namespace std;
 //Space complexity: O(1) as no extra space was used in sorting of array
 void bubble_sort(int arr[], int n) {
     for (int i=n-1; i>=0; i--) {
+        int didSwap = 0;
         for (int j=0; j <= i-1; j++) {
             if (arr[j] > arr[j+1]) {
                 int temp = arr[j + 1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
+                didSwap = 1;
             }
+        }
+        // We will check in the first iteration if any swap is taking place. 
+        //If the array is already sorted no swap will occur and we will break out from the loops. 
+        if (didSwap == 0) { 
+            break;
         }
     }
 
--- a-z_Striver sheet/Sorting/Merge_sort.cpp
+++ a-z_Striver sheet/Sorting/Merge_sort.cpp
@@ -1,2 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//Time Complexity: O(N*LOG(N)) for worst and average case and best case....At each step, we divide the whole array, for that 
+//logn and we assume n steps are taken to get sorted array, so overall time complexity will be nlogn
+//Space complexity: O(N) as we are using a temporary array to store elements in sorted order.
+void merge(int arr[], int low, int mid, int high)
+{
+    vector<int> temp; 
+    int left=low;
+    int right=mid+1;
+    while(left<=mid && right<=high)
+    {
+      if (arr[left] <= arr[right]) {
+        temp.push_back(arr[left]);
+        left++;
+      }
+      else{
+          temp.push_back(arr[right]);
+          right++;
+      }
+    }
+    while(left<=mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++)
+    {
+        arr[i]=temp[i-low];
+    }
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if(l==r) return;
+    int mid=(l+r)/2;
+    // Write Your Code Here
+    mergeSort(arr,l,mid);
+    mergeSort(arr,mid+1,r);
+    merge(arr,l,mid,r);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    cout << endl;
+    mergeSort(arr, 0, n);
+    for (int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout << endl;
+    return 0;
+
+}
